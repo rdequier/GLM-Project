554aae8827456af8a8e921f7fd6f444a91528f59
rdequier <rdequier@gmail.com>
2024-03-21 12:00:42 +0100

    Added new model

--- ProjectAttempt1.R
+++ ProjectAttempt1.R
@@ -13,6 +13,8 @@ library(topmodels)
 library(MASS)
 library(sandwich)
 library(lmtest)
+library(tidyr)
+
 
 ######Functions########
 
@@ -31,6 +33,7 @@ pearson.test= function(model){
   return(data.frame(X2s=X2, pvalue=(1-pchisq(X2,n-p))))
 }
 
+#deviance test function
 deviance.test = function(model){
   dev=deviance(model)
   res.df = fit1$df.residual
@@ -38,28 +41,11 @@ deviance.test = function(model){
 }
 
 
-#Function to plot deviance residuals vs linear predictors:
-linpred_vs_dev = function(model){
-model.rdev<-residuals(model,type="deviance")
-plot(predict(model),model.rdev,xlab="Linear predictor", ylab="Devianceresiduals")
-abline(h=0,lty=2,col="grey")
-loess.model.rdev <- loess(model.rdev~predict(model))
-model.lo.pred.dev <- predict(loess.model.rdev, se=T)
-j.model<- order(predict(model))
-lines(predict(model)[j.model],model.lo.pred.dev$fit[j.model],col="blue",lwd=3)
-lines(predict(model)[j.model],model.lo.pred.dev$fit[j.model]
-      +2*model.lo.pred.dev$s[j.model], lty=2,col="red")
-lines(predict(model)[j.model],model.lo.pred.dev$fit[j.model]
-      -2*model.lo.pred.dev$s[j.model], lty=2,col="red")
-}
-
-
-
 ##### Processing Data ######
 
 
 getwd()
-setwd("C:/Users/rdequ/Documents/R/GLM")
+setwd("C:/Users/rdequ/Documents/Github/GLM-Project")
 
 eba = read.table("eba1977.txt")
 
@@ -89,7 +75,9 @@ plot(eba$age, eba$cases/eba$pop)
 plot(eba$city, eba$cases/eba$pop)
 
 
-
+#Grouped histograms
+ggplot(eba, aes(cases, fill=city))+
+  geom_histogram(position="dodge")
 
 
 #### Basic Model With Offset ####              
@@ -116,17 +104,62 @@ Anova(fit1, test = "LR",type=3)
 #Wald
 Anova(fit1, test = "Wald",type=3)
 
-#Deviance residuals vs linear predictor:
-linpred_vs_dev(fit1)
-
-
 #Overdispersion
-dispersiontest(fit1, alternative ="less")
 dispersiontest(fit1)
 
+#estimating dispersion
+sum(residuals(fit1, type="pearson")^2)/(fit1$df.residual)
 
 
 
+#### Residual Plots #####
+
+#Pearson residuals vs fitted values
+resid = resid(fit1, type="pearson")
+fitted=fitted(fit1)
+ggplot(eba, aes(fitted, resid)) +
+  geom_point() +
+  ylab("Pearsons residuals") + xlab("Fitted values")+
+  geom_smooth()+
+  geom_hline(yintercept=0, col="red", lty=2)
+
+#Pearson residuals vs linear predictors
+resid = resid(fit1, type="pearson")
+linpred=fit1$linear.predictors
+ggplot(eba, aes(linpred, resid)) +
+  geom_point() +
+  ylab("Pearsons residuals") + xlab("Linear Predictors")+
+  geom_smooth()+
+  geom_hline(yintercept=0, col="red", lty=2)
+
+#Deviance residuals vs fitted values#
+resid = resid(fit1, type="deviance")
+fitted=fitted(fit1)
+ggplot(eba, aes(fitted, resid)) +
+  geom_point() +
+  ylab("Deviance residuals") + xlab("Fitted values")+
+  geom_smooth()+
+  geom_hline(yintercept=0, col="red", lty=2)
+
+#Pearson residuals vs linear predictors
+resid = resid(fit1, type="deviance")
+linpred=fit1$linear.predictors
+ggplot(eba, aes(linpred, resid)) +
+  geom_point() +
+  ylab("Deviance residuals") + xlab("Linear Predictors")+
+  geom_smooth()+
+  geom_hline(yintercept=0, col="red", lty=2)
+
+
+#influentail observations:
+influencePlot(fit1)
+
+
+#Unsure what else we can add to this
+
+
+
+#### Plotting the 
 #### DHARMa residuals to check for over dispersion ####
 sim.fit1 = simulateResiduals(fit1, plot=T)
 
@@ -160,6 +193,8 @@ summary(fit1)
 #### Negative binomial model #####
 fit1.nb=glm.nb(cases ~ city + age + offset(log(pop)), data = eba)
 summary(fit1.nb)
+
+
 #this model does not converge:
 #My guess is because the estimate of of dispersion is 1/theta, therefore
 #becuase there is no quadratic trend, theta is getting a huge values
@@ -171,9 +206,100 @@ coeftest(fit1, vcov=sandwich)
 
 #Now the cities of Kolding and Vejle are significant.. hmmm
 
+#### Fredericia vs other cities: Justification ######
+summary(fit1)
+beta = coef(fit1)
+#Plot lines of cancer rates by city:
+#Fredericia:
+F = c(exp(beta[1]),
+      exp(beta[1]+beta[5]),
+      exp(beta[1]+beta[6]),
+      exp(beta[1]+beta[7]),
+      exp(beta[1]+beta[8]),
+      exp(beta[1]+beta[9]))
+
+#Horsens
+H = c(exp(beta[1]+beta[2]),
+      exp(beta[1]+beta[2]+beta[5]),
+      exp(beta[1]+beta[2]+beta[6]),
+      exp(beta[1]+beta[2]+beta[7]),
+      exp(beta[1]+beta[2]+beta[8]),
+      exp(beta[1]+beta[2]+beta[9]))
+
+#Kolding
+K = c(exp(beta[1]+beta[3]),
+      exp(beta[1]+beta[3]+beta[5]),
+      exp(beta[1]+beta[3]+beta[6]),
+      exp(beta[1]+beta[3]+beta[7]),
+      exp(beta[1]+beta[3]+beta[8]),
+      exp(beta[1]+beta[3]+beta[9]))
+
+#Vejle
+
+V = c(exp(beta[1]+beta[4]),
+      exp(beta[1]+beta[4]+beta[5]),
+      exp(beta[1]+beta[4]+beta[6]),
+      exp(beta[1]+beta[4]+beta[7]),
+      exp(beta[1]+beta[4]+beta[8]),
+      exp(beta[1]+beta[4]+beta[9]))
+
+minmax = range(c(F,H,K,V))
+
+graph_data = data.frame(x=1:6, Fredericia=F, Horsens=H, Kolden=K, Vejle = V)
+
+graph_data = graph_data %>% pivot_longer(cols=c("Fredericia", "Horsens",
+                                                "Kolden", "Vejle"),
+                                         names_to="city",
+                                         values_to = "rates")
+
+labels = c("[45-54]","[55,59]","[60,64]","[65,69]","[70,74]","75+")
+
+ggplot(graph_data, aes(x=x, y=rates))+
+  geom_line(aes(color=city), size=1)+
+  geom_point(aes(color=city))+
+  scale_color_manual(name='City', labels=c("Fredericia", "Horsens",
+                                            "Kolden", "Vejle"),
+                     values=c('red', 'purple', 'steelblue','green'))+
+  scale_x_continuous(breaks=1:6, labels=labels)+
+  xlab("Age Range") + ylab("Predicted Cancer Rates") +
+  ggtitle("Cancer Rates by City")
+
+
+#Our model shows that Frederic is higher than the other three cities,
+# but only Kolden is significant in the model
+
+#Create a new column in the datafame:
+eba$Fredereica = ifelse(eba$city=="Fredericia", "Fredericia", "Other")
+
+#### Fredericia model #####
+fit2 <- glm(cases~Fredereica+age+offset(log(pop)),
+            family=poisson, data=eba)
+summary(fit2)
+
+#comparing the two models
+anova(fit1,fit2, test="Chisq")
+
+####Gof Tests
+#pearson:
+pearson.test(fit2)
+
+#Deviance Test:
+deviance.test(fit2)
+
+#Holy trinty
+#LR test
+Anova(fit2, test = "LR",type=3)
+
+#Wald
+Anova(fit2, test = "Wald",type=3)
+
+#dispersion estimate
+sum(residuals(fit2, type="pearson")^2)/(fit1$df.residual)
+
+
 #### plotting the mean vs the variance #####
 
-#Not sure if this is right....?
+#Not sure if this is right or even necessary#
 xb <- predict(fit1.nb)
 g <- cut(xb, breaks=quantile(xb,seq(0,100,10)/100))
 m <- tapply(eba$cases, g, mean)
@@ -188,15 +314,7 @@ legend("topleft", lty=c("dashed","solid"),legend=c("Q. Poisson","Neg. Binom."), 
 
 
 
-#### Model with Fredericia as the only city ####
-fit2 = glm(cases~1+offset(log(pop)),
-           family=poisson(link="log"),
-           data=eba)
 
-summary(fit2)
-
-
-#Not sure how to do this.
 
 
 #### Rootograms #######
@@ -206,10 +324,12 @@ rootogram(fit1,
           main = "Poisson")
 
 rootogram(fit1.nb)
+
+rootogram(fit2)
 #### Model Selection #####
 
-models = list(fit1, fit1.nb)
-mod.names = c("basic", "nb")
+models = list(fit1, fit2)
+mod.names = c("basic", "fredericia_vs_other")
 aictab(cand.set=models, modnames=mod.names)
 
 
